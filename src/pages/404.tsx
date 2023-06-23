d496d4c4ded578083db36e1e48a875cd4be32dfa
Gabriel Barreto <gh.barreto@hotmail.com>
2023-06-22 20:05:21 -0700

    added instagram links

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import Home from './';
 
 export default function PageDoesNotExist() {
-    // returns main page if page does not exist
+    // returns main page if page does not exist.
     return <Home />;
 }
