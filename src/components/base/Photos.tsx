6fe1a1780d862635234a8885b9b91a0e9f88dce1
Gabriel Barreto <gh.barreto@hotmail.com>
2023-06-28 16:34:55 -0700

    feat: add photos inside accordion

--- src/components/base/Photos.tsx
+++ src/components/base/Photos.tsx
@@ -8,6 +8,11 @@ import styled from 'styled-components';
 import { useCallback, useState } from 'react';
 import ImageViewer from 'react-simple-image-viewer';
 import AspectRatioIcon from '@mui/icons-material/AspectRatio';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 import { Container } from './Container';
 
 const StyledImage = styled.img`
@@ -15,6 +20,32 @@ const StyledImage = styled.img`
     object-fit: cover;
 `;
 
+const StyledTitle = styled.p`
+    font-family: var(--font-family);
+    line-height: 30px;
+    font-weight: bold;
+    color: var(--white);
+`;
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1300px;
+    margin: 0 auto;
+    border-bottom: 0;
+    color: var(--white);
+
+    overflow: scroll;
+
+    .Mui-expanded {
+        background-color: var(--primary);
+    }
+
+    @media (max-width: 768px) {
+        max-width: 90%;
+    }
+`;
 const StyledImageList = styled(ImageList)`
     .react-simple-image-viewer {
         &__next,
@@ -104,7 +135,7 @@ export const Photos = () => {
         } else if (window.innerWidth >= 768) {
             setColsPerView(3);
         } else {
-            setColsPerView(2);
+            setColsPerView(1);
         }
     };
 
@@ -136,41 +167,57 @@ export const Photos = () => {
 
     return (
         <Container height={'100%'}>
-            <StyledImageList
-                sx={{
-                    margin: '0 auto',
-                    maxWidth: { lg: '90%', xl: '90%' },
-                }}
-                cols={colsPerView}
-                rowHeight={200}
-            >
-                {itemData.map((item, index) => (
-                    <ImageListItem key={index} onClick={() => openImageViewer(index)}>
-                        <StyledImage src={item.img} srcSet={item.img} alt={item.title} loading="lazy" />
-                        <ImageListItemBar
-                            title={item.title}
-                            subtitle={item.author}
-                            onClick={() => openImageViewer(index)}
-                            actionIcon={
-                                <IconButton sx={{ color: 'var(--white)' }} aria-label={`info about ${item.title}`}>
-                                    <AspectRatioIcon />
-                                </IconButton>
-                            }
-                        />
-                    </ImageListItem>
-                ))}
+            <StyledContainer>
+                <Accordion TransitionProps={{ unmountOnExit: true }} sx={{ backgroundColor: 'var(--primary)' }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon style={{ color: 'var(--white' }} />}
+                        aria-controls={`photo-content`}
+                        id={`photo-header`}
+                    >
+                        <StyledTitle>View Photos</StyledTitle>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <StyledImageList
+                            sx={{
+                                margin: '0 auto',
+                                maxWidth: { lg: '90%', xl: '90%' },
+                            }}
+                            cols={colsPerView}
+                            rowHeight={200}
+                        >
+                            {itemData.map((item, index) => (
+                                <ImageListItem key={index} onClick={() => openImageViewer(index)}>
+                                    <StyledImage src={item.img} srcSet={item.img} alt={item.title} loading="lazy" />
+                                    <ImageListItemBar
+                                        title={item.title}
+                                        subtitle={item.author}
+                                        onClick={() => openImageViewer(index)}
+                                        actionIcon={
+                                            <IconButton
+                                                sx={{ color: 'var(--white)' }}
+                                                aria-label={`info about ${item.title}`}
+                                            >
+                                                <AspectRatioIcon />
+                                            </IconButton>
+                                        }
+                                    />
+                                </ImageListItem>
+                            ))}
 
-                {isViewerOpen && (
-                    <ImageViewer
-                        backgroundStyle={{ backgroundColor: 'rgba(0,0,0,0.7)' }}
-                        src={images}
-                        currentIndex={currentImage}
-                        disableScroll
-                        closeOnClickOutside={true}
-                        onClose={closeImageViewer}
-                    />
-                )}
-            </StyledImageList>
+                            {isViewerOpen && (
+                                <ImageViewer
+                                    backgroundStyle={{ backgroundColor: 'rgba(0,0,0,0.7)' }}
+                                    src={images}
+                                    currentIndex={currentImage}
+                                    disableScroll
+                                    closeOnClickOutside={true}
+                                    onClose={closeImageViewer}
+                                />
+                            )}
+                        </StyledImageList>
+                    </AccordionDetails>
+                </Accordion>
+            </StyledContainer>
         </Container>
     );
 };
