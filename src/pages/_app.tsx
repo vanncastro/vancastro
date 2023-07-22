3db5ec0b83965dd40f47bfd0af470d156954bc70
Gabriel Barreto <gh.barreto@hotmail.com>
2023-07-21 18:33:51 -0700

    background on slider arrows

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import './root_vars.scss';
-import '../components/base/ReviewCard.css';
+import '../components/base/ReviewCard.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
