66b244c2593d4ac44fcb3c205f0eb92014201490
Gabriel Barreto <gh.barreto@hotmail.com>
2024-02-14 21:36:33 -0800

    remove elaine yoshida

--- src/components/Partnerships.tsx
+++ src/components/Partnerships.tsx
@@ -4,48 +4,40 @@ import Image from 'next/image';
 import { Container } from './base/Container';
 
 const StyledContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    gap: 20px;
-    flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+  flex-wrap: wrap;
 
-    @media (max-width: 768px) {
-        display: block;
-    }
+  @media (max-width: 768px) {
+    display: block;
+  }
 `;
 
 const StyledImage = styled(Image)`
-    object-fit: contain;
-    margin: 40px 0 100px;
-    border-radius: 5px;
-    border: 2px solid var(--primary);
-    box-shadow: var(--primary) 0px 20px 30px -10px;
+  object-fit: contain;
+  margin: 40px 0 100px;
+  border-radius: 5px;
+  border: 2px solid var(--primary);
+  box-shadow: var(--primary) 0px 20px 30px -10px;
 
-    @media (max-width: 768px) {
-        display: block;
-        width: 80%;
-        height: 40%;
-        margin: 20px auto 0;
-    }
+  @media (max-width: 768px) {
+    display: block;
+    width: 80%;
+    height: 40%;
+    margin: 20px auto 0;
+  }
 `;
 
 export const Partnerships = () => {
-    return (
-        <Container
-            title={'Partners'}
-            id="Partners"
-            hasLightBackground
-            darkTitleColor
-            height="100%"
-            paddingBottom={'40px'}
-        >
-            <StyledContainer>
-                <StyledImage src="/partnership/elaine_yoshida.jpg" width={400} height={400} alt="elaine yoshida" />
-                <a href="https://facebook.com/icbcknowledgetestmaterial" target="_blank">
-                    <StyledImage src="/partnership/kika_.jpg" width={400} height={400} alt="kika" />
-                </a>
-                <StyledImage src="/partnership/debora.jpeg" width={400} height={400} alt="debora " />
-            </StyledContainer>
-        </Container>
-    );
+  return (
+    <Container title={'Partners'} id="Partners" hasLightBackground darkTitleColor height="100%" paddingBottom={'40px'}>
+      <StyledContainer>
+        <a href="https://facebook.com/icbcknowledgetestmaterial" target="_blank">
+          <StyledImage src="/partnership/kika_.jpg" width={400} height={400} alt="kika" />
+        </a>
+        <StyledImage src="/partnership/debora.jpeg" width={400} height={400} alt="debora " />
+      </StyledContainer>
+    </Container>
+  );
 };
