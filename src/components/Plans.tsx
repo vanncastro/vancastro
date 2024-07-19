9026b52337073a3af76913bd925cc4c9c0716f50
Gabriel Barreto <gh.barreto@hotmail.com>
2024-07-18 18:42:32 -0700

    switched plan image prices

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -52,7 +52,7 @@ export const Plans = () => {
         <Container title="plans" id="plans" darkTitleColor height={'100%'}>
             <StyledDiv>
                 <Image src="/plans2.jpeg" alt="plans" width={800} height={40} />
-                <Image src="/plans1.jpeg" alt="plans" width={800} height={40} />
+                <Image src="/plans1.png" alt="plans" width={800} height={40} />
                 {/* {pricePlans.map(plan => (
                     <StyledPriceCard key={plan.id}>
                         <StyledIcon>{plan.icon}</StyledIcon>
